cc50f8ffe78706a396348e85fe19f2f909243021
apple <apple@appledeMacBook-Pro.local>
2022-11-01 16:25:30 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-
-import App from './App';
 import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+ 
+// 使用createRoot
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
+ 
+// ReactDOM.render(  // 原来的代码
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
